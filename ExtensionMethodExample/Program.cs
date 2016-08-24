5e120c6f7b82f270ebd84decf2abb98ae4a0b6dd
Thiti Yamsung <thiti.y@cdg.co.th>
2016-08-24 15:48:27 +0700

    First commit.
    Signed-off-by: Thiti Yamsung <thiti.y@cdg.co.th>

---
+++ ExtensionMethodExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExample {
+
+    class Program {
+
+        static void Main(string[] args) {
+            int x = 20;
+            Console.WriteLine(string.Format("x={0} is Even number: {1}", x, x.isEvenNumber()));
+            Console.Read();
+        }
+
+    }
+
+    static class ExtensionTest {
+
+        public static bool isEvenNumber(this int a) {
+            return a % 2 == 0 ? true : false;
+        }
+
+    }
+
+}
